648778c3645df0151700ba7f55fab0caf85e4d29
Cardinal <cardinalby@gmail.com>
2024-03-12 00:29:53 +0100

    try chown on linux systems

--- pkg/build_go/start.go
+++ pkg/build_go/start.go
@@ -2,6 +2,7 @@ package build_go
 
 import (
 	"fmt"
+	"os"
 	"path"
 
 	xgolib "github.com/cardinalby/xgo-as-library"
@@ -10,6 +11,7 @@ import (
 	"github.com/cardinalby/xgo-pack/pkg/pipeline/buildctx"
 	fsutil "github.com/cardinalby/xgo-pack/pkg/util/fs"
 	"github.com/cardinalby/xgo-pack/pkg/util/logging"
+	osutil "github.com/cardinalby/xgo-pack/pkg/util/os"
 	typeutil "github.com/cardinalby/xgo-pack/pkg/util/type"
 )
 
@@ -46,6 +48,17 @@ func Start(
 		}
 
 		outBinPath := getXgoOutBinPath(config.BinTempDir, outBinPrefix, target)
+
+		uid, gid, ok, err := osutil.GetLinuxUser()
+		if err != nil {
+			return err
+		}
+		if ok {
+			if err := os.Chown(outBinPath, uid, gid); err != nil {
+				return fmt.Errorf("error changing owner of xgo build result bin '%s': %w", outBinPath, err)
+			}
+		}
+
 		if err := fsutil.RenameFile(outBinPath, tConfig.OutBinPath); err != nil {
 			return fmt.Errorf(
 				"error moving xgo build result bin '%s' to '%s': %w. It may be a result of unsuccessful build",
